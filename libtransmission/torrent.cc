atomwh/transmission

commit c5c2bebfebf65d0dfca441a3845a47b53270c090

libtransmission: add recheck verify mode option
* add option to determine whether using fast verify mode for rechecking

Signed-off-by: Hui Wang <wanghui@palmatom.com>

libtransmission/torrent.cc
@@ -1829,6 +1829,14 @@ void tr_torrentVerify(tr_torrent* tor)
     tor->session->runInSessionThread(verifyTorrent, tor);
 }
 
+void tr_torrentVerifyRecheck(tr_torrent* tor)
+{
+    if (tor->session->shouldFullyVerifyRecheckTorrents() || !torrent_init_helpers::isNewTorrentASeed(tor))
+    {
+        tr_torrentVerify(tor);
+    }
+}
+
 void tr_torrent::setVerifyState(tr_verify_state state)
 {
     TR_ASSERT(state == TR_VERIFY_NONE || state == TR_VERIFY_WAIT || state == TR_VERIFY_NOW);

libtransmission/quark.cc
@@ -18,7 +18,7 @@ using namespace std::literals;
 namespace
 {
 
-auto constexpr MyStatic = std::array<std::string_view, 401>{ ""sv,
+auto constexpr MyStatic = std::array<std::string_view, 402>{ ""sv,
                                                              "activeTorrentCount"sv,
                                                              "activity-date"sv,
                                                              "activityDate"sv,
@@ -374,6 +374,7 @@ auto constexpr MyStatic = std::array<std::string_view, 401>{ ""sv,
                                                              "torrent-complete-sound-enabled"sv,
                                                              "torrent-duplicate"sv,
                                                              "torrent-get"sv,
+                                                             "torrent-recheck-verify-mode"sv,
                                                              "torrent-set"sv,
                                                              "torrent-set-location"sv,
                                                              "torrentCount"sv,

libtransmission/quark.h
@@ -377,6 +377,7 @@ enum
     TR_KEY_torrent_complete_sound_enabled,
     TR_KEY_torrent_duplicate,
     TR_KEY_torrent_get,
+    TR_KEY_torrent_recheck_verify_mode,
     TR_KEY_torrent_set,
     TR_KEY_torrent_set_location,
     TR_KEY_torrentCount,

libtransmission/rpcimpl.cc
@@ -297,7 +297,7 @@ char const* torrentVerify(tr_session* session, tr_variant* args_in, tr_variant* 
 {
     for (auto* tor : getTorrents(session, args_in))
     {
-        tr_torrentVerify(tor);
+        tr_torrentVerifyRecheck(tor);
         session->rpcNotify(TR_RPC_TORRENT_CHANGED, tor);
     }
 

libtransmission/session-settings.h
@@ -72,7 +72,8 @@ struct tr_variant;
     V(TR_KEY_umask, umask, tr_mode_t, 022, "") \
     V(TR_KEY_upload_slots_per_torrent, upload_slots_per_torrent, size_t, 8U, "") \
     V(TR_KEY_utp_enabled, utp_enabled, bool, true, "") \
-    V(TR_KEY_torrent_added_verify_mode, torrent_added_verify_mode, tr_verify_added_mode, TR_VERIFY_ADDED_FAST, "")
+    V(TR_KEY_torrent_added_verify_mode, torrent_added_verify_mode, tr_verify_added_mode, TR_VERIFY_ADDED_FAST, "") \
+    V(TR_KEY_torrent_recheck_verify_mode, torrent_recheck_verify_mode, tr_verify_recheck_mode, TR_VERIFY_RECHECK_FAST, "")
 
 struct tr_session_settings
 {

libtransmission/session.h
@@ -717,6 +717,11 @@ public:
         return settings_.torrent_added_verify_mode == TR_VERIFY_ADDED_FULL;
     }
 
+    [[nodiscard]] constexpr auto shouldFullyVerifyRecheckTorrents() const noexcept
+    {
+        return settings_.torrent_recheck_verify_mode == TR_VERIFY_RECHECK_FULL;
+    }
+
     [[nodiscard]] constexpr auto shouldDeleteSource() const noexcept
     {
         return settings_.should_delete_source_torrents;

libtransmission/transmission.h
@@ -68,6 +68,12 @@ enum tr_verify_added_mode
     TR_VERIFY_ADDED_FULL = 1
 };
 
+enum tr_verify_recheck_mode
+{
+    TR_VERIFY_RECHECK_FAST = 0,
+    TR_VERIFY_RECHECK_FULL = 1
+};
+
 enum tr_preallocation_mode
 {
     TR_PREALLOCATE_NONE = 0,
@@ -1406,6 +1412,7 @@ void tr_torrentAmountFinished(tr_torrent const* torrent, float* tab, int n_tabs)
  * Queue a torrent for verification.
  */
 void tr_torrentVerify(tr_torrent* torrent);
+void tr_torrentVerifyRecheck(tr_torrent* torrent);
 
 bool tr_torrentHasMetadata(tr_torrent const* tor);
 

libtransmission/variant-converters.cc
@@ -44,6 +44,11 @@ auto constexpr VerifyModeKeys = std::array<std::pair<std::string_view, tr_verify
     { "fast", TR_VERIFY_ADDED_FAST },
     { "full", TR_VERIFY_ADDED_FULL },
 } };
+
+auto constexpr VerifyRecheckModeKeys = std::array<std::pair<std::string_view, tr_verify_recheck_mode>, 2>{ {
+    { "fast", TR_VERIFY_RECHECK_FAST },
+    { "full", TR_VERIFY_RECHECK_FULL },
+} };
 } // namespace
 
 namespace libtransmission
@@ -350,6 +355,53 @@ template<>
 void VariantConverter::save<tr_verify_added_mode>(tr_variant* tgt, tr_verify_added_mode const& val)
 {
     for (auto const& [key, value] : VerifyModeKeys)
+    {
+        if (value == val)
+        {
+            tr_variantInitStrView(tgt, key);
+            return;
+        }
+    }
+
+    tr_variantInitInt(tgt, val);
+}
+
+template<>
+std::optional<tr_verify_recheck_mode> VariantConverter::load<tr_verify_recheck_mode>(tr_variant* src)
+{
+    static constexpr auto& Keys = VerifyRecheckModeKeys;
+
+    if (auto val = std::string_view{}; tr_variantGetStrView(src, &val))
+    {
+        auto const needle = tr_strlower(tr_strvStrip(val));
+
+        for (auto const& [name, value] : Keys)
+        {
+            if (name == needle)
+            {
+                return value;
+            }
+        }
+    }
+
+    if (auto val = int64_t{}; tr_variantGetInt(src, &val))
+    {
+        for (auto const& [name, value] : Keys)
+        {
+            if (value == val)
+            {
+                return value;
+            }
+        }
+    }
+
+    return {};
+}
+
+template<>
+void VariantConverter::save<tr_verify_recheck_mode>(tr_variant* tgt, tr_verify_recheck_mode const& val)
+{
+    for (auto const& [key, value] : VerifyRecheckModeKeys)
     {
         if (value == val)
         {

commit a070d06dc7cf3c7caad949ce1f80683c0ceb1c3e

libtransmission: fix progress update when rechecking
Fixes: c5c2bebfe ("libtransmission: add recheck verify mode option")
Signed-off-by: Hui Wang <wanghui@palmatom.com>

libtransmission/torrent.cc
@@ -1835,6 +1835,12 @@ void tr_torrentVerifyRecheck(tr_torrent* tor)
     {
         tr_torrentVerify(tor);
     }
+    else
+    {
+        tor->completion.setHasAll();
+        tor->doneDate = tr_time();
+        tor->recheckCompleteness();
+    }
 }
 
 void tr_torrent::setVerifyState(tr_verify_state state)